b3f88eedc5917760a31b764dc44a162936da7568
Shen Lim <lim.shen@outlook.com>
2018-10-30 01:47:34 -0500

    Scatterplot improvements

--- beta.R
+++ beta.R
@@ -44,13 +44,13 @@ beta <- function(symbol,index,from,to,period) {
                                        col_rename = "Rb")),
                        by = c("date" = "date")) %>%
                ggplot(aes(x = Rb, y = Ra)) +
-               geom_point(color = palette_light()[[1]], alpha = 1, na.rm = TRUE) +
-               geom_smooth(method = "lm", level = FALSE, na.rm = TRUE) +
+               geom_point(color = "orange1", alpha = 1, na.rm = TRUE) +
+               geom_smooth(method = "lm", color = "cyan", level = FALSE, na.rm = TRUE) +
                labs(title = paste(symbol, "Returns Regressed on", index, "Returns"),
                     x = paste(index, "Returns"),
                     y = paste(symbol, "Returns")) +
-               geom_vline(aes(xintercept = 0)) +
-               geom_hline(aes(yintercept = 0)) +
+               geom_vline(aes(xintercept = 0), color = "ghostwhite", size = 0.75) +
+               geom_hline(aes(yintercept = 0), color = "ghostwhite", size = 0.75) +
                xlim((abs(max((tq_get(index,
                                      get  = "stock.prices",
                                      from = from,
@@ -59,7 +59,7 @@ beta <- function(symbol,index,from,to,period) {
                                              mutate_fun = periodReturn,
                                              period     = period,
                                              type       = "arithmetic",
-                                             col_rename = "Rb"))$Rb)) * -1.01),
+                                             col_rename = "Rb"))$Rb)) * -1.005),
                     (abs(max((tq_get(index,
                                      get  = "stock.prices",
                                      from = from,
@@ -68,7 +68,7 @@ beta <- function(symbol,index,from,to,period) {
                                              mutate_fun = periodReturn,
                                              period     = period,
                                              type       = "arithmetic",
-                                             col_rename = "Rb"))$Rb)) * 1.01)) +
+                                             col_rename = "Rb"))$Rb)) * 1.005)) +
                ylim((abs(max((tq_get(symbol,
                                      get  = "stock.prices",
                                      from = from,
@@ -77,7 +77,7 @@ beta <- function(symbol,index,from,to,period) {
                                              mutate_fun = periodReturn,
                                              period     = period,
                                              type       = "arithmetic",
-                                             col_rename = "Ra"))$Ra)) * -1.01),
+                                             col_rename = "Ra"))$Ra)) * -1.005),
                     (abs(max((tq_get(symbol,
                                      get  = "stock.prices",
                                      from = from,
@@ -86,7 +86,11 @@ beta <- function(symbol,index,from,to,period) {
                                              mutate_fun = periodReturn,
                                              period     = period,
                                              type       = "arithmetic",
-                                             col_rename = "Ra"))$Ra)) * 1.01)) +
-               theme_tq())
+                                             col_rename = "Ra"))$Ra)) * 1.005)) +
+               theme(panel.background = element_rect(fill   = "black",
+                                                     colour = "black",
+                                                     size   = 0.5, linetype = "solid"),
+                     panel.grid.major = element_line(size = 0.5, linetype = 'solid', colour = "gray50"), 
+                     panel.grid.minor = element_line(size = 0.25, linetype = 'solid', colour = "gray50")))
   return(list(ggplot, capm.beta))
 }
